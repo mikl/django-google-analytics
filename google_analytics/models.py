f886cbd846b7d7a95a78f86241309a5c9b864bd3
Igor Davydenko <playpauseandstop@gmail.com>
2009-05-12 16:04:20 +0300

    Sets ``django-google-analytics`` compatible with Django >= 1.0.

--- google_analytics/models.py
+++ google_analytics/models.py
@@ -1,11 +1,18 @@
-from django.db import models
 from django.conf import settings
-from django.contrib.sites.models import Site
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 if getattr(settings, 'GOOGLE_ANALYTICS_MODEL', False):
     class Analytics(models.Model):
-        site = models.ForeignKey(Site, edit_inline=models.TABULAR, max_num_in_admin=1, min_num_in_admin=1)
-        analytics_code = models.CharField(blank=True, max_length=100, core=True)
+        site = models.OneToOneField('sites.Site', verbose_name=_('site'))
+        code = models.CharField(_('analytics code'), blank=True,
+            max_length=100)
+
+        class Meta:
+            db_table = 'google_analytics'
+            verbose_name = _('Google Analytics')
+            verbose_name_plural = _('Google Analytics')
 
         def __unicode__(self):
-            return u"%s" % (self.analytics_code)
+            return self.code
---
+++ google_analytics/tests.py
@@ -0,0 +1,142 @@
+import unittest
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.test import Client
+
+
+if getattr(settings, 'GOOGLE_ANALYTICS_MODEL', False):
+    from google_analytics.models import Analytics
+
+
+    TEST_CODE = 'UA-123456-7'
+    TEST_CODE_ANOTHER = 'UA-765432-1'
+    TEST_EMAIL = 'email@domain.com'
+    TEST_PASSWORD = 'password'
+    TEST_USERNAME = 'username'
+
+
+    class TestAnalytics(unittest.TestCase):
+
+        def setUp(self):
+            # Creates admin user
+            admin = User.objects.create_user(username=TEST_USERNAME,
+                                             password=TEST_PASSWORD,
+                                             email=TEST_EMAIL)
+            admin.is_staff, admin.is_superuser = True, True
+            admin.save()
+
+            # Clean-up previous analytics codes
+            Analytics.objects.all().delete()
+
+            self.admin = admin
+            self.client = Client()
+            self.site = Site.objects.get_current()
+
+        def tearDown(self):
+            self.admin.delete()
+
+        def test_admin(self):
+            client = self.client
+            site = self.site
+
+            client.login(username=TEST_USERNAME, password=TEST_PASSWORD)
+            response = client.get('/admin/sites/site/%d/' % site.pk)
+            self.assertEqual(response.status_code, 200)
+
+            html = response.content
+
+            assert 'Google Analytics' in html, html
+            assert 'Analytics code' in html, html
+            assert 'name="analytics-0-code"' in html, html
+
+            # Adds Google Analytics code to current site
+            response = client.post('/admin/sites/site/%d/' % site.pk,
+                                   {
+                                       'name': site.name,
+                                       'domain': site.domain,
+                                       'analytics-INITIAL_FORMS': '0',
+                                       'analytics-TOTAL_FORMS': '1',
+                                       'analytics-0-id': '0',
+                                       'analytics-0-site': '%d' % site.pk,
+                                       'analytics-0-code': TEST_CODE,
+                                   })
+            self.assertEqual(response.status_code, 302)
+
+            analytics = Analytics.objects.get(code=TEST_CODE)
+            self.assertEqual(analytics.site, site)
+
+            # Edits Google Analytics code of current site
+            response = client.post('/admin/sites/site/%d/' % site.pk,
+                                   {
+                                       'name': site.name,
+                                       'domain': site.domain,
+                                       'analytics-INITIAL_FORMS': '1',
+                                       'analytics-TOTAL_FORMS': '1',
+                                       'analytics-0-id': '%d' % analytics.pk,
+                                       'analytics-0-site': '%d' % site.pk,
+                                       'analytics-0-code': TEST_CODE_ANOTHER,
+                                   },
+                                   follow=True)
+            self.assertEqual(response.status_code, 302)
+
+            analytics = Analytics.objects.get(code=TEST_CODE_ANOTHER)
+            self.assertEqual(analytics.site, site)
+
+            # Clean-up Google Analytics code
+            response = client.post('/admin/sites/site/%d/' % site.pk,
+                                   {
+                                       'name': site.name,
+                                       'domain': site.domain,
+                                       'analytics-INITIAL_FORMS': '1',
+                                       'analytics-TOTAL_FORMS': '1',
+                                       'analytics-0-id': '%d' % analytics.pk,
+                                       'analytics-0-site': '%d' % site.pk,
+                                       'analytics-0-code': '',
+                                   })
+            self.assertEqual(response.status_code, 302)
+
+            analytics = Analytics.objects.get(code='')
+            self.assertEqual(analytics.site, site)
+
+            # Deletes Google Analytics
+            old_counter = Analytics.objects.count()
+            response = client.post('/admin/sites/site/%d/' % site.pk,
+                                   {
+                                       'name': site.name,
+                                       'domain': site.domain,
+                                       'analytics-INITIAL_FORMS': '1',
+                                       'analytics-TOTAL_FORMS': '1',
+                                       'analytics-0-DELETE': 'on',
+                                       'analytics-0-id': '%d' % analytics.pk,
+                                       'analytics-0-site': '%d' % site.pk,
+                                       'analytics-0-code': '',
+                                   })
+            self.assertEqual(response.status_code, 302)
+
+            new_counter = Analytics.objects.count()
+
+            self.assertEqual(new_counter + 1, old_counter)
+            self.assertRaises(Analytics.DoesNotExist,
+                              Analytics.objects.get,
+                              site=site)
+
+        def test_models(self):
+            old_counter = Analytics.objects.count()
+            analytics = Analytics.objects.create(site=self.site,
+                                                 code=TEST_CODE)
+
+            new_counter = Analytics.objects.count()
+            self.assertEqual(new_counter - 1, old_counter)
+            self.assertEqual(unicode(analytics), u'%s' % TEST_CODE)
+
+            analytics.code = ''
+            analytics.save()
+
+            self.assertEqual(unicode(analytics), u'')
+
+            self.assertRaises(Exception,
+                              Analytics.objects.create,
+                              site=self.site,
+                              code=TEST_CODE_ANOTHER)
